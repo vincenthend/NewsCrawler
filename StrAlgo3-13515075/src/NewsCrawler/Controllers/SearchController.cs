21a738ab2f446afd8928a7104ebaf8991cd0fbdc
vincenthend <vincenthendrha@gmail.com>
2017-04-28 08:53:10 +0700

    Finish

---
+++ StrAlgo3-13515075/src/NewsCrawler/Controllers/SearchController.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+using System.Web.Mvc;
+using NewsCrawler.Models;
+using NewsCrawler.Controllers;
+using MySql.Data.Entity;
+using System.Data.Entity;
+
+namespace NewsCrawler.Controllers {
+  public class SearchController : Controller {
+
+    /**
+    * Mencari string pada newsList dengan urutan judul baru kemudian isi.
+    * 
+    * @param newsList list berita dari database
+    * @param searchQuery search pattern yang digunakan
+    * @return List berita yang lolos search dengan lokasi ditemukannya
+    */
+    public ActionResult Index(string searchQuery, int searchType) {
+      DbConfiguration.SetConfiguration(new MySqlEFConfiguration());
+      String connectionString = "server=127.0.0.1;User Id=root;password=password;database=db_newscrawler";
+      MySqlConnection connection = new MySqlConnection(connectionString);
+
+      NewsCrawlerDB dbNews = new NewsCrawlerDB(connection, false);
+      dbNews.Database.CreateIfNotExists();
+
+      connection.Open();
+
+      // Empty tables
+      MySqlCommand cmd = new MySqlCommand("delete from News", connection);
+      cmd.ExecuteNonQuery();
+      dbNews.SaveChanges();
+
+      MySqlTransaction transaction = connection.BeginTransaction();
+      dbNews.Database.UseTransaction(transaction);
+      // Get data from RSS
+      Loader.loadRSS("http://rss.detik.com/index.php/detikcom", dbNews);
+      Loader.loadRSS("http://tempo.co/rss/terkini", dbNews);
+      Loader.loadRSS("http://rss.vivanews.com/get/all", dbNews);
+      transaction.Commit();
+
+      // Convert news list to array
+      News[] newsArray = dbNews.News.SqlQuery("select * from News").ToArray();
+      List<NewsFound> newsFound = new List<NewsFound>();
+
+      // Do Search
+      if (searchType == 0) {
+        newsFound = searchKMP(newsArray, searchQuery);
+      }
+      else if (searchType == 1) {
+        newsFound = searchBM(newsArray, searchQuery);
+      }
+      else if (searchType == 2) {
+        newsFound = searchRegex(newsArray, searchQuery);
+      }
+      else if (searchType == 3) {
+        //debug to show all news
+        newsFound = showAll(newsArray);
+      }
+
+      ViewBag.regexQuery = Searcher.regexConvert(searchQuery);
+      ViewBag.searchQuery = searchQuery;
+      ViewBag.searchType = searchType;
+      ViewBag.searchResult = newsFound;
+      ViewBag.searchCount = newsFound.Count;
+
+      return View();
+    }
+
+    /**
+     * Mencari string pada newsList dengan algoritma KMP judul baru kemudian isi.
+     * 
+     * @param newsList list berita dari database
+     * @param searchQuery search pattern yang digunakan
+     * @return List berita yang lolos search dengan lokasi ditemukannya
+     */
+    private List<NewsFound> searchKMP(News[] newsList, string searchQuery) {
+      int i;
+      int foundLoc;
+      List<NewsFound> foundList = new List<NewsFound>();
+
+      // Begin searching in all newsList
+      for (i = 0; i < newsList.Length; i++) {
+        // Search Title
+        foundLoc = Searcher.KMPSearchFirst(newsList[i].title, searchQuery);
+        if (foundLoc == -1) {
+          // Search Description
+          Searcher.KMPSearchFirst(newsList[i].content, searchQuery);
+          if (foundLoc != -1) {
+            // Found in description
+            if (newsList[i].content.Length >= 50 + foundLoc) {
+              newsList[i].content = newsList[i].content.Substring(foundLoc, 49) + "...";
+            }
+            else {
+              newsList[i].content = newsList[i].content.Substring(foundLoc);
+            }
+
+            foundList.Add(new NewsFound(newsList[i], foundLoc, false));
+          }
+        }
+        else {
+          // Found in title
+          if (newsList[i].content.Length >= 50) {
+            newsList[i].content = newsList[i].content.Substring(0, 49) + "...";
+          }
+
+          foundList.Add(new NewsFound(newsList[i], foundLoc, true));
+        }
+      }
+
+      return foundList;
+    }
+
+    /**
+     * Mencari string pada newsList dengan algoritma BM judul baru kemudian isi.
+     * 
+     * @param newsList list berita dari database
+     * @param searchQuery search pattern yang digunakan
+     * @return List berita yang lolos search dengan lokasi ditemukannya
+     */
+    private List<NewsFound> searchBM(News[] newsList, string searchQuery) {
+      int i;
+      int foundLoc;
+      List<NewsFound> foundList = new List<NewsFound>();
+
+      // Begin searching in all newsList
+      for (i = 0; i < newsList.Length; i++) {
+        // Search Title
+        foundLoc = Searcher.BMSearchFirst(newsList[i].title, searchQuery);
+        if (foundLoc == -1) {
+          // Search Description
+          Searcher.BMSearchFirst(newsList[i].content, searchQuery);
+          if (foundLoc != -1) {
+            // Found in description
+            if (newsList[i].content.Length >= 50 + foundLoc) {
+              newsList[i].content = newsList[i].content.Substring(foundLoc, 49) + "...";
+            }
+            else {
+              newsList[i].content = newsList[i].content.Substring(foundLoc);
+            }
+            foundList.Add(new NewsFound(newsList[i], foundLoc, false));
+          }
+        }
+        else {
+          // Found in Title
+          if (newsList[i].content.Length >= 50) {
+            newsList[i].content = newsList[i].content.Substring(0, 49) + "...";
+          }
+          foundList.Add(new NewsFound(newsList[i], foundLoc, true));
+        }
+      }
+
+      return foundList;
+    }
+
+    /**
+     * Mencari string pada newsList dengan regex judul baru kemudian isi.
+     * 
+     * @param newsList list berita dari database
+     * @param searchQuery search pattern yang digunakan
+     * @return List berita yang lolos search dengan lokasi ditemukannya
+     */
+    private List<NewsFound> searchRegex(News[] newsList, string searchQuery) {
+      int i;
+      int foundLoc;
+      List<NewsFound> foundList = new List<NewsFound>();
+
+      // Begin searching in all newsList
+      for (i = 0; i < newsList.Length; i++) {
+        // Search Title
+        foundLoc = Searcher.RegexSearchFirst(newsList[i].title, searchQuery);
+        if (foundLoc == -1) {
+          // Search Description
+          Searcher.RegexSearchFirst(newsList[i].content, searchQuery);
+          if (foundLoc != -1) {
+            // Found in description
+            foundList.Add(new NewsFound(newsList[i], foundLoc, false));
+            if (newsList[i].content.Length >= 50 + foundLoc) {
+              newsList[i].content = newsList[i].content.Substring(foundLoc, 49) + "...";
+            }
+            else {
+              newsList[i].content = newsList[i].content.Substring(foundLoc);
+            }
+          }
+        }
+        else {
+          // Found in Title
+          foundList.Add(new NewsFound(newsList[i], foundLoc, true));
+          if (newsList[i].content.Length >= 50) {
+            newsList[i].content = newsList[i].content.Substring(0, 49) + "...";
+          }
+        }
+      }
+
+      return foundList;
+    }
+
+    /**
+     * Menampilkan newsList
+     * 
+     * @param newsList list berita dari database
+     * @return List semua berita
+     */
+    private List<NewsFound> showAll(News[] newsList) {
+      int i;
+      int foundLoc = 0;
+      List<NewsFound> foundList = new List<NewsFound>();
+
+      // Begin searching in all newsList
+      for (i = 0; i < newsList.Length; i++) {
+        {
+          foundList.Add(new NewsFound(newsList[i], foundLoc, true));
+        }
+      }
+
+      return foundList;
+    }
+  }
+}
---
+++ StrAlgo3-13515075/src/NewsCrawler/Controllers/Searcher.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text.RegularExpressions;
+
+namespace NewsCrawler.Controllers {
+  public class Searcher {
+    public static int KMPSearchFirst(string text, string pattern) {
+      int n;
+      int m;
+      int i;
+      int j;
+      int[] fail;
+      text = text.ToLower();
+      pattern = pattern.ToLower();
+      n = text.Length;
+      m = pattern.Length;
+      i = 0;
+      j = 0;
+      fail = computeFail(pattern);
+      while (i < n) {
+        if (text[i] == pattern[j]) {
+          if (j == m - 1) {
+            return (i - m + 1);
+          }
+          i++;
+          j++;
+          if (j >= m) {
+            j = fail[j - 1];
+          }
+        }
+        else if (j > 0) {
+          j = fail[j - 1];
+        }
+        else {
+          i++;
+        }
+      }
+      return -1;
+    }
+
+    public static int[] KMPSearchAll(string text, string pattern) {
+      int n;
+      int m;
+      int i;
+      int j;
+      int[] fail;
+      List<int> idxList;
+      text = text.ToLower();
+      pattern = pattern.ToLower();
+      n = text.Length;
+      m = pattern.Length;
+      i = 0;
+      j = 0;
+      fail = computeFail(pattern);
+      idxList = new List<int>();
+      while (i < n) {
+        if (text[i] == pattern[j]) {
+          if (j == m - 1) {
+            idxList.Add(i - m + 1);
+          }
+          i++;
+          j++;
+          if (j >= m) {
+            j = fail[j - 1];
+          }
+        }
+        else if (j > 0) {
+          j = fail[j - 1];
+        }
+        else {
+          i++;
+        }
+      }
+      return idxList.ToArray();
+    }
+
+    public static int BMSearchFirst(string text, string pattern) {
+      List<int> retVal = new List<int>();
+      int m = pattern.Length;
+      int n = text.Length;
+
+      int[] badChar = new int[256];
+
+      BadCharHeuristic(pattern, m, ref badChar);
+
+      int s = 0;
+      while (s <= (n - m)) {
+        int j = m - 1;
+
+        while (j >= 0 && pattern[j] == text[s + j])
+          --j;
+
+        if (j < 0) {
+          return s;
+        }
+        else {
+          s += Math.Max(1, j - badChar[text[s + j]]);
+        }
+      }
+
+      return -1;
+    }
+
+    public static int[] BMSearchAll(string text, string pattern) {
+      List<int> retVal = new List<int>();
+      int m = pattern.Length;
+      int n = text.Length;
+
+      int[] badChar = new int[256];
+
+      BadCharHeuristic(pattern, m, ref badChar);
+
+      int s = 0;
+      while (s <= (n - m)) {
+        int j = m - 1;
+
+        while (j >= 0 && pattern[j] == text[s + j])
+          --j;
+
+        if (j < 0) {
+          retVal.Add(s);
+          s += (s + m < n) ? m - badChar[text[s + m]] : 1;
+        }
+        else {
+          s += Math.Max(1, j - badChar[text[s + j]]);
+        }
+      }
+
+      return retVal.ToArray();
+    }
+
+    public static int RegexSearchFirst(string text, string pattern) {
+      string regexPattern = regexConvert(pattern);
+      Regex regex = new Regex(regexPattern);
+
+      Match matchFound = regex.Match(text);
+
+      if (matchFound.Success) {
+        return matchFound.Index;
+      }
+      else {
+        return -1;
+      }
+    }
+    
+    private static void BadCharHeuristic(string text, int size, ref int[] badChar) {
+      int i;
+
+      for (i = 0; i < 256; i++)
+        badChar[i] = -1;
+
+      for (i = 0; i < size; i++)
+        badChar[(int)text[i]] = i;
+    }
+
+    private static int[] computeFail(string pattern) {
+      int[] fail;
+      int m;
+      int i;
+      int j;
+      fail = new int[pattern.Length];
+      fail[0] = 0;
+      i = 1;
+      j = 0;
+      m = pattern.Length;
+      while (i < m) {
+        if (pattern[i] == pattern[j]) {
+          fail[i] = j + 1;
+          i++;
+          j++;
+        }
+        else if (j > 0) {
+          j = fail[j - 1];
+        }
+        else {
+          fail[i] = 0;
+          i++;
+        }
+      }
+      return fail;
+    }
+
+    public static string regexConvert(string text) {
+      string[] regexList;
+      string tempString = "";
+      string regexConcat = "";
+
+      //Split per word
+      regexList = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+      // Create regex
+      int i;
+      int j;
+      for (i = 0; i < regexList.Length; i++) {
+        if (regexList[i].Length > 3) {
+          regexList[i] = regexList[i];
+        }
+        else {
+          for (j = 0; j < regexList[i].Length; j++) {
+            tempString = tempString + regexList[i].ElementAt(j) + "\\w* *";
+          }
+          regexList[i] = tempString;
+        }
+      }
+
+      //Combine regex
+      regexConcat = regexList[0];
+      for (i = 1; i < regexList.Length; i++) {
+        regexConcat = regexConcat + "|" + regexList[i];
+      }
+      regexConcat = "(?i:" + regexConcat + ")";
+      return regexConcat;
+    }
+
+  }
+}
